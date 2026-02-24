8a8b6bded3ac941b0f73a342dc7556230636311b
Danno <danielcintao@gmail.com>
2026-02-24 12:27:42 +0000

    1.1.6

--- ex_1.1.6.c
+++ ex_1.1.6.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <math.h>
+const float pi = 3.14159;
 
 int main(){
     int ang, min, sec;
+    float graus_sum, graus_rad;
+    printf("Introduza o ângulo, minutos e segundos: ");
+    scanf("%d %d %d", &ang, &min, &sec);
+    ang = ang % 360;
+    if (ang < 0){
+        ang = ang + 360;
+    }
+    graus_sum = ang + (min/60.0) + (sec/3600.0);
+    graus_rad = graus_sum * (pi/180.0);
+    printf("Em radianos é %f.\n", graus_rad);
+    return 0;
 }
