ee7d5fe123cdab84e7101f178a8d7e20fe00b529
Danno <danielcintao@gmail.com>
2026-03-11 20:09:00 +0000

    alterações_5.1.1

--- chap_5/ex_5.1.1.c
+++ chap_5/ex_5.1.1.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 void transformar(){
-    int vet[5];
-    int i;
-    for (i = 0; i < 5; i++){
+    int i, size;
+    
+    printf("Introduza o tamanho do array: \n");
+    scanf("%d", &size);
+    int vet[size];
+
+    for (i = 0; i < size; i++){
         printf("Introduza os valores do vet de cada vez: \n");
         scanf("%d", &vet[i]);
     }
     printf("O vetor alterado é: ");
-    for(i = 0; i < 5; i++){
+    for(i = 0; i < size; i++){
         if (vet[i] % 2 == 1){
             vet[i] = vet[i] * 2;
         }
