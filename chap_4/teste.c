95483e3ff0f7b06b35abd6941bbca1b4d5e94a0d
Danno <danielcintao@gmail.com>
2026-03-12 11:47:11 +0000

    ex_ponteiros_aula

--- chap_4/teste.c
+++ chap_4/teste.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 
-typedef struct{
-    int numi;
-    char nome[20];
-    int idade;
-} Empregado;
+// typedef struct{
+//     int numi;
+//     char nome[20];
+//     int idade;
+// } Empregado;
 
+// int main(){
+//     Empregado e;
+//     // Empregado.e[100];
+//     e.idade = 21;
+
+//     printf("A idade é %d.\n", e.idade);
+// }
 int main(){
-    Empregado e;
-    // Empregado.e[100];
-    e.idade = 21;
-
-    printf("A idade é %d.\n", e.idade);
+    int i = 3;
+    int *p;                     // p é um ponteiro;
+    p = &i;                     // em p está o endereço de i;
+    printf("Output: %d\n", *p); // print do valor guardado em i;
+    printf("Output: %p\n", p);  // print do endereço de i;
 }
+
+// void swap(int *a, int *b){
+//     int temp;
+//     temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+
+// int main(){
+//     int i, j, *a, *b;;
+//     i = 2;
+//     j = 4;
+//     a = &i;
+//     b = &j;
+//     swap(a, b);
+//     printf("%d %d\n", i, j);
+// }
--- chap_5/ex_5.2.3.c
+++ chap_5/ex_5.2.3.c
@@ -12,10 +12,6 @@ void sort(int v[], int n){
             }
         }
     }
-    for (i = 0; i < n; i++){
-        printf("%d ", v[i]);
-    }
-    printf("\n");
 }
 
 int main(){
@@ -23,21 +19,33 @@ int main(){
     printf("Introduza o tamanho do array: \n");
     scanf("%d", &size1);
     int vet1[size1];
-
+    
     for (i = 0; i < size1; i++){
         printf("Introduza os valores do vet de cada vez: \n");
         scanf("%d", &vet1[i]);
     }
-
+    
     printf("Introduza o tamanho do array: \n");
     scanf("%d", &size2);
     int vet2[size2];
-
+    
     for (i = 0; i < size2; i++){
         printf("Introduza os valores do vet de cada vez: \n");
         scanf("%d", &vet2[i]);
     }
     sort(vet1, size1);
     sort(vet2, size2);
-
+    
+    int size_f = size1 + size2;
+    int vet_f[size_f];
+    for (i = 0; i < size1; i++){
+        vet_f[i] = vet1[i];
+    }
+    for (i = size1; i < size_f - 1; i++){
+        vet_f[i] = vet2[i];
+    }
+    for (i = 0; i < size_f; i++){
+        printf("%d ", vet_f[i]);
+    }
+    printf("\n");
 }
