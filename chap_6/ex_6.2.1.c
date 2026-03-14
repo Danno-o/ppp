12b817a7a76960d9219f27c01a2a7adb6b4f3bd3
Danno <danielcintao@gmail.com>
2026-03-14 18:05:24 +0000

    6.2.1 inacabado

---
+++ chap_6/ex_6.2.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void encriptar(char *a){
+    char x[26] = 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+    char y[26] = 'D','E','I','A','B','C','F','G','H','J','K','L','Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M'
+    for(int i = 0; i < strlen(a); i++){
+        toupper(a[i]);
+    }
+    printf()
+}
