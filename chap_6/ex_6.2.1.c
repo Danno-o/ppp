d35ab41863e470885998b113f6ed682f5765cfbe
Danno <danielcintao@gmail.com>
2026-03-14 22:06:54 +0000

    ex_6.2.1

--- chap_6/ex_6.2.1.c
+++ chap_6/ex_6.2.1.c
@@ -3,10 +3,46 @@
 #include <ctype.h>
 
 void encriptar(char *a){
-    char x[26] = 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-    char y[26] = 'D','E','I','A','B','C','F','G','H','J','K','L','Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M'
-    for(int i = 0; i < strlen(a); i++){
-        toupper(a[i]);
+    char x[26] = "DEIABCFGHJKLZYXWVUTSRQPONM";
+    int index;
+
+    for(int i = 0; a[i] != '\0'; i++){
+        a[i] = (char)toupper(a[i]);
     }
-    printf()
+    for(int j = 0; j < a[j]; j++){
+        if (isalpha(a[j])){
+            index = a[j] - 'A';
+            a[j] = x[index];
+        }
+    }
+    printf("%s\n", a);
+}
+
+void desencriptar(char *a){
+    char y[26] = "DEIABCFGHJKLZYXWVUTSRQPONM";
+    char inv[26];   
+    int index;
+    int pos;
+
+    for(int i = 0; a[i] != '\0'; i++){
+        a[i] = (char)toupper(a[i]);
+    }
+    for (int l = 0; l < 26; l++){
+        pos = y[l] - 'A';
+        inv[pos] = 'A' + l;
+    }
+    for(int j = 0; j < y[j]; j++){
+        if (isalpha(a[j])){
+            index = a[j] - 'A';
+            a[j] = inv[index];
+        }
+    }
+    printf("%s\n", a);
+}
+
+int main(){
+    char string[50] = "Boas malta";  // string[] funciona, mas *string não, porquê?
+    char string2[50] = "DEIABCFGHJKLZYXWVUTSRQPONM";
+    encriptar(string);
+    desencriptar(string2);
 }
