efcd320879ca2c1995cbcaf1a1babc2b9a0a838d
Danno <danielcintao@gmail.com>
2026-04-20 00:02:15 +0100

    chap_10_cont

--- chap_10/ex_10.1.2.c
+++ chap_10/ex_10.1.2.c
@@ -1,3 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 3
 
+
+
+void valsMatriz(int *matriz){
+    printf("Matriz do tipo %dX%d.\n", N, N);
+    printf("Digite os valores da matriz: \n");
+    for (int i = 0; i < N*N; i++){
+        scanf("%d", &matriz[i]);
+    }
+}
+
+void matrizConstr(int *matriz){    
+    int posAtual = 0;
+    printf("A matriz é: \n");
+    for (int i = 0; i < N; i++){
+        for (int k = posAtual; k < posAtual + N; k++){
+            printf("%d\t", matriz[k]);
+        }
+        printf("\n");
+        posAtual += N;
+    }
+}
+
+int main(){
+    int *matriz = malloc((N*N) * sizeof(int));
+    valsMatriz(matriz);
+    matrizConstr(matriz);
+}
